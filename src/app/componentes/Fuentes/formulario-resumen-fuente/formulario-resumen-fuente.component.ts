851e785c9637c745d7f8346e01650f9b294d83c3
Hector <vix.hector@gmail.com>
2018-06-22 11:39:08 +0200

    agregando tema a fuente

--- src/app/componentes/Fuentes/formulario-resumen-fuente/formulario-resumen-fuente.component.ts
+++ src/app/componentes/Fuentes/formulario-resumen-fuente/formulario-resumen-fuente.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import  {FuenteModelo} from  '../formulario-resumen-fuente/fuente-modelo';
+import { FuenteModelo } from '../formulario-resumen-fuente/fuente-modelo';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-formulario-resumen-fuente',
@@ -8,37 +8,98 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormularioResumenFuenteComponent implements OnInit {
   fuenteForm: FormGroup;
-  fuentemodelo:FuenteModelo;
-  archivado =['','CDC', 'ZOOLOGIA', 'BOTANICA', 'SECCION LIBROS','TESIS','NO EN CASA'];
-  naturalezaDocument=['','A', 'C', 'F', 'I','D','L','M','O','P','R'];
-  
+  fuentemodelo: FuenteModelo;
+  archivado = ['', 'CDC', 'ZOOLOGIA', 'BOTANICA', 'SECCION LIBROS', 'TESIS', 'NO EN CASA'];
+  naturalezaDocument = ['', 'A', 'C', 'F', 'I', 'D', 'L', 'M', 'O', 'P', 'R'];
+
 
 
   constructor(private fb: FormBuilder) {
     this.createForm();
-      
+
   }
-  createForm (){
+  createForm() {
 
     this.fuenteForm = this.fb.group({
-        'codfuente': [null, Validators.minLength(3)],
-        'cita': [null, Validators.minLength(5)],
-        'archivado': ''
+      'codfuente': [null, Validators.minLength(3)],
+      'cita': [null, Validators.minLength(5)],
+      'archivado': '',
+      'cobgeo': '',
+      'resumen': [null, Validators.minLength(10)],
+      'comunat': '',
+      'comunterr': '',
+      'bosque': '',
+      'sabana': '',
+      'prado': '',
+      'chaparral': '',
+      'desierto': '',
+      'alpino': '',
+      'otroterr': '',
+      'comunac': '',
+      'palustre': '',
+      'lacustre': '',
+      'pluvial': '',
+      'estuarino': '',
+      'maritimo': '',
+      'subterp': '',
+      //flora
+      'flora': '',
+      'floraac': '',
+      'floraterp': '',
+      'plnovasc': '',
+      'microorg': '',
+      'infositio': '',
+      //fauna
+      'fauna': '',
+      'faunaac': '',
+      'faunaterr': '',
+      'moluscos': '',
+      'insectos': '',
+      'crustaceos': '',
+      'otroantrop': '',
+      'otroinvert': '',
+      'peces': '',
+      'anfibios': '',
+      'reptiles': '',
+      'aves': '',
+      'mamiferos': '',
+      'cienfisic': '',
+      'fisiotopo': '',
+      //otros
+      'hidrol    ': '',
+      'geologia  ': '',
+      'suelos    ': '',
+      'clima     ': '',
+      'biologia  ': '',
+      'ecologia  ': '',
+      'funecol   ': '',
+      'diversnat ': '',
+      'inventario': '',
+      'tecinvest ': '',
+      'am        ': '',
+      'planmanejo': '',
+      'tecmanejo ': '',
+      'estimpamb ': '',
+      'organprot ': '',
+      'herrprot  ': ''
+
+
     });
   }
-   
-  resetForm (){
+
+
+  resetForm() {
 
     this.fuenteForm.reset();
   }
-  
+
 
   ngOnInit() {
 
   }
 
   onSubmit() {
-    
-      console.log("Form Submitted!");
-    }
+
+    console.log("Form Submitted!");
+  }
 }
