63781c73a5558386207f3bf578a4d24622409542
Hector <vix.hector@gmail.com>
2018-07-05 14:52:03 -0600

    Integrando ngb-tab

--- src/app/componentes/jerarquizacion/jerarquizacion.component.ts
+++ src/app/componentes/jerarquizacion/jerarquizacion.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbTabset} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-jerarquizacion',
   templateUrl: './jerarquizacion.component.html',
-  styleUrls: ['./jerarquizacion.component.scss']
+  styleUrls: ['./jerarquizacion.component.scss'],
+
 })
 export class JerarquizacionComponent implements OnInit {
-
-  constructor() { }
+  constructor( ) { }
 
   ngOnInit() {
   }
