1e2055520ea4cbf1269e9d074fccc5456a40e3c9
Hector <vix.hector@gmail.com>
2018-07-04 20:04:51 -0600

    Integrando alertas

--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
-//import { UsuarioModelo } from '../login-page/usuario-modelo';
 import { UsuarioService } from '../../servicios/usuario.service';
 import { Router } from '@angular/router';
-import { UsuarioModelo,Rol } from '../../modelo/usuario-modelo';
+import { UsuarioModelo, Rol } from '../../modelo/usuario-modelo';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { DatePipe } from '@angular/common'
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 
 const now = new Date();
 @Component({
@@ -23,110 +25,108 @@ export class LoginPageComponent implements OnInit {
   us: UsuarioModelo[];
   model: NgbDateStruct;
   date: { year: number, month: number };
-   emailConfirmar:string;
+  emailConfirmar: string;
   //pruebas
   data: any;
-  fecha: Date ;
+  fecha: Date;
   modelo2: NgbDateStruct;
-   rol : Rol;
+  rol: Rol;
+  private _success = new Subject<string>();
+  staticAlertClosed = false;
+  successMessage: string;
+  tipoAlert: string;
+
   constructor(public fb: FormBuilder,
     private usuarioService: UsuarioService,
     private router: Router,
     public fb2: FormBuilder,
     public fb3: FormBuilder,
-    public datepipe: DatePipe)
-     {
+    public datepipe: DatePipe) {
     this.loginForm = fb.group({
       LFemail: ['', [Validators.required, Validators.email]],
       LFcontrasena: ['', Validators.required],
-      //db: new FormControl()
     });
 
     this.crearRegistroForm();
     this.restablecerContrasenaForm();
   }
   ngOnInit() {
+    setTimeout(() => this.staticAlertClosed = true, 20000);
+    this._success.subscribe((message) => this.successMessage = message);
+    this._success.pipe(
+      debounceTime(5000)
+    ).subscribe(() => this.successMessage = null);
   }
+
+  //logiarse
   onSubmit() {
 
-    console.log(this.loginForm.value);
-
-    //this.router.navigate(['/home']);
-   // this.addUsuario(this.loginForm.value);
-   this.usuarioService.getUsuarioDatos()
-     .subscribe(data => {
-      this.data = data;
-      console.log(this.data);
-  
-    });
+    this.usuarioService.getUsuarioDatos()
+      .subscribe(
+        data => {
+          this.data = data;
+          console.log(this.data);
+          this.router.navigate(['/home']);
+        }, err => {
+          this.changeSuccessMessage('No se pudo logiar, servidor no disponible.', 'warning ');
+        });
 
   }
   //Guardar Registro
-  onSubmitRegistro(){
-
-
-    console.log(this.registroForm.value);
-    this.modelo2=this.registroForm.get('fechaNacimiento').value;
-  // this.fecha=new Date();
-   // let latest_date =this.datepipe.transform(this.fecha, 'yyyy-MM-dd');
-     this.fecha=this.toModel(this.modelo2);
-     
-     console.log(this.registroForm.value);
-     this.addUsuario(this.registroForm.value);
-  //  this.addUsuario(this.registroForm.value);
+  onSubmitRegistro() {
+    this.modelo2 = this.registroForm.get('fechaNacimiento').value;
+    this.fecha = this.toModel(this.modelo2);
+    this.addUsuario(this.registroForm.value);
   }
 
   addUsuario(usDatos: UsuarioModelo): void {
-     console.log(usDatos.fechaNacimiento);
-     ;
-     usDatos.fechaNacimiento=  this.fecha;
-     usDatos.usuarioId=1000000000;
+    usDatos.fechaNacimiento = this.fecha;
+    this.rol = new Rol();
+    this.rol.rolId = 1;
+    usDatos.rolrolid = this.rol;
 
-     this.rol=new Rol();
-     this.rol.rolId=123;
-    // usDatos.rolrolid=this.rol;
-     console.log(usDatos.fechaNacimiento);
-      
+    this.usuarioService.addUsuario(usDatos)
+      .subscribe(
+        us => {
+          this.changeSuccessMessage(`Registro exitoso:${us.nombre}.`, 'success');
+        }, err => {
+          this.changeSuccessMessage('No se pudo registrar, servidor no disponible.', 'warning ');
+        });
 
-   this.usuarioService.addUsuario(usDatos)
-      .subscribe(us => {
-
-        console.log('bbb:' + us.nombre);
-      });
-      
   }
 
 
   rebuildForm() {
     this.loginForm.reset({
-
     });
 
   }
 
   crearRegistroForm() {
-
     this.registroForm = this.fb2.group({
       nombre: ['', Validators.required],
       apellido: ['', Validators.required],
       fechaNacimiento: ['', Validators.required],
       email: ['', [Validators.email, Validators.required]],
-   //   emailConfirmar: ['', [Validators.email, Validators.required]],
       contrasena: ['', Validators.required],
 
     });
   }
   restablecerContrasenaForm() {
     this.recuperarContrasenaForm = this.fb3.group({
-      emailRecuperarContrasena:  ['', [Validators.email, Validators.required]]
-
-
+      emailRecuperarContrasena: ['', [Validators.email, Validators.required]]
     });
   }
   selectToday() {
     this.model = { year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate() };
   }
+
   toModel(date: NgbDateStruct): Date {
-    return date ? new Date(''+date.year+'-'+date.month+'-'+date.day) : null;
+    return date ? new Date('' + date.year + '-' + date.month + '-' + date.day) : null;
+  }
+
+  public changeSuccessMessage(mensaje: string, tipo: string) {
+    this.tipoAlert = tipo;
+    this._success.next(mensaje);
   }
 }
--- src/app/servicios/usuario.service.ts
+++ src/app/servicios/usuario.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Response } from "@angular/http";
-import { Observable , of } from 'rxjs';
-import {UsuarioModelo} from '../modelo/usuario-modelo';
+import { Observable, of } from 'rxjs';
+import { UsuarioModelo } from '../modelo/usuario-modelo';
 
 import { catchError, map, tap } from 'rxjs/operators';
 
@@ -17,18 +17,18 @@ const httpOptions = {
 })
 export class UsuarioService {
   readonly rootUrl = 'http://localhost:8080/cdc/rs';
-  data : any;
+  data: any;
 
   constructor(private http: HttpClient) { }
 
-  
+
 
   userAuthentication(userName, password) {
-    
+
     var data = "username=" + userName + "&password=" + password + "&grant_type=password";
-    var reqHeader = new HttpHeaders({ 'Content-Type': 'application/x-www-urlencoded','No-Auth':'True' });
-   
-    const req  =this.http.post(this.rootUrl + '/us/token', { headers: reqHeader })
+    var reqHeader = new HttpHeaders({ 'Content-Type': 'application/x-www-urlencoded', 'No-Auth': 'True' });
+
+    const req = this.http.post(this.rootUrl + '/us/token', { headers: reqHeader })
       .subscribe(
         res => {
           console.log(res);
@@ -39,30 +39,22 @@ export class UsuarioService {
       );
   }
 
-  getUsuarioDatos(){
-    console.log('getUs');
-    return this.http.get('http://192.168.1.2:8080/cdc/rs/us/') ;
-    /*.subscribe(data => {
-      this.data = data;
-      console.log(this.data);
-  
-    });*/
-  
-   }
-   addUsuario (us: UsuarioModelo): Observable<UsuarioModelo> {
-    return this.http.post<UsuarioModelo>(this.rootUrl + '/us/reg', us, httpOptions).pipe(
-      tap((us: UsuarioModelo) => console.log(`added us w/ nombre=${us.nombre}`)),
-      catchError(this.handleError<UsuarioModelo>('addUsuario'))
-    );
+  getUsuarioDatos() {
+    return this.http.get(this.rootUrl + '/us/');
+
   }
- 
-  private handleError<T> (operation = 'operation', result?: T) {
+  addUsuario(us: UsuarioModelo): Observable<UsuarioModelo> {
+    return this.http.post<UsuarioModelo>(this.rootUrl + '/us/reg', us, httpOptions);
+  }
+
+  //para capturar los errores con el HttpClient
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-     console.error(error); 
+      console.error(error);
 
       return of(result as T);
     };
   }
 
-   }
+}
 
