80e0fee692f3426ce33d64309671f43cb3507fb1
Hector <vix.hector@gmail.com>
2018-07-06 00:36:00 -0600

    Diseño y alineamiento de formularios

--- src/app/componentes/jerarquizacion/formulario-jerarquizacion/formulario-jerarquizacion.component.ts
+++ src/app/componentes/jerarquizacion/formulario-jerarquizacion/formulario-jerarquizacion.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-formulario-jerarquizacion',
   templateUrl: './formulario-jerarquizacion.component.html',
   styleUrls: ['./formulario-jerarquizacion.component.scss']
 })
 export class FormularioJerarquizacionComponent implements OnInit {
-
-  constructor() { }
-
+  lnaciones = ['','GT','US'];
+  jerarquizacionForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.createFormJerarquizacionNacional();
+   }
+  
   ngOnInit() {
+  }
+  createFormJerarquizacionNacional() {
+    this.jerarquizacionForm = this.fb.group({
+      'codigoe': ['', Validators.required],
+      'cobgeo': '',
+      
+    });
   }
 
 }
