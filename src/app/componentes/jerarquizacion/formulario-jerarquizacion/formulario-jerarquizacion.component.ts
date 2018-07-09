2439b5965e5245baa036a47cebd2702089943ad4
Hector <vix.hector@gmail.com>
2018-07-09 15:39:34 -0600

    Diseño completo jerarquización

--- src/app/componentes/jerarquizacion/formulario-jerarquizacion/formulario-jerarquizacion.component.ts
+++ src/app/componentes/jerarquizacion/formulario-jerarquizacion/formulario-jerarquizacion.component.ts
@@ -1,27 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { DatePipe } from '@angular/common'
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+const now = new Date();
 @Component({
   selector: 'app-formulario-jerarquizacion',
   templateUrl: './formulario-jerarquizacion.component.html',
   styleUrls: ['./formulario-jerarquizacion.component.scss']
 })
 export class FormularioJerarquizacionComponent implements OnInit {
-  lnaciones = ['','GT','US'];
-  jerarquizacionForm: FormGroup;
-  constructor(private fb: FormBuilder) {
-    this.createFormJerarquizacionNacional();
-   }
+  lnacion = ['', 'GT', 'US'];
+  lnestim= ['', '', ''];
+  lnabund= ['', '', ''];
+  lndist= ['', '', ''];
+  lnleprot= ['', '', ''];
+  lnamenaz= ['', '', ''];
+  lrangon= ['', '', ''];
   
+  jerarquizacionForm: FormGroup;
+  date: { year: number, month: number };
+  modelDate: NgbDateStruct;
+  constructor(
+    private fb: FormBuilder,
+    public datepipe: DatePipe
+  ) {
+    this.createFormJerarquizacionNacional();
+  }
+
   ngOnInit() {
   }
   createFormJerarquizacionNacional() {
     this.jerarquizacionForm = this.fb.group({
+      //pagina1
       'codigoe': ['', Validators.required],
-      'cobgeo': '',
-      'nlestimcom': ''
-      
-      
+      'nombren': '',
+      'nacion': '',
+      'nloctip': '',
+      'nlestim': '',
+      'nlestimcom': '',
+      'nabund': '',
+      'nabundcom': '',
+      'ndist': '',
+      'ndistcom': '',
+      'nleprot': '',
+      'nleprotcom': '',
+      'namenaz': '',
+      'namenazcom': '',
+      //pagina2
+      'notroconsi': '',
+      'rangon': '', 'fecharn': '',
+      'nranrzon': '',
+      'nnecprotec': '',
+      'nnecinvent': '',
+      'nnecestudi': '',
+      'nnecmaejo': '',
+      'resrn': '',
+      'edautor': '',
+      'edicion': '',
+      'actualizar': ''
     });
+  }
+  selectToday() {
+    this.modelDate = { year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate() };
   }
 
 }
