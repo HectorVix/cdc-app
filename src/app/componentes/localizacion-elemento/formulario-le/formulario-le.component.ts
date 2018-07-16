01b110cde33d69787a10a3a6526f22bb94f508fe
Hector <vix.hector@gmail.com>
2018-07-16 16:34:48 -0600

    Implementando  input-group-append  en el diseño

--- src/app/componentes/localizacion-elemento/formulario-le/formulario-le.component.ts
+++ src/app/componentes/localizacion-elemento/formulario-le/formulario-le.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { DatePipe } from '@angular/common'
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+const now = new Date();
 @Component({
   selector: 'app-formulario-le',
   templateUrl: './formulario-le.component.html',
@@ -7,9 +12,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioLeComponent implements OnInit {
   lident = ['','S','N','?'];
+  date: { year: number, month: number };
+  modelDate: NgbDateStruct;
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectToday() {
+    this.modelDate = { year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate() };
+  }
 }
