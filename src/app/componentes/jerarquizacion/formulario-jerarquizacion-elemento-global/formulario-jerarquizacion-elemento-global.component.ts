ed94aee8ef0f898ca8852d9241d7ce7ae63918f5
Hector <vix.hector@gmail.com>
2018-08-01 12:10:01 -0600

    Diseño Jerarquización elemento global Exitoso

--- src/app/componentes/jerarquizacion/formulario-jerarquizacion-elemento-global/formulario-jerarquizacion-elemento-global.component.ts
+++ src/app/componentes/jerarquizacion/formulario-jerarquizacion-elemento-global/formulario-jerarquizacion-elemento-global.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-jerarquizacion-elemento-global.component.scss']
 })
 export class FormularioJerarquizacionElementoGlobalComponent implements OnInit {
-
+ labundg = ['','A','B'];
+ ldistg = ['','A','B'];
+ ltendenciag = ['','A','B'];
+ lleprotg = ['','A','B'];
+ lamenazg = ['','A','B'];
+ lfragilg = ['','A','B'];
+ lrangog = ['','A','B'];
   constructor() { }
 
   ngOnInit() {
