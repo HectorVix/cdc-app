46f09e8f3624adfd81664efcd525451d976f4e1d
Hector <vix.hector@gmail.com>
2018-08-05 21:04:01 -0600

    Integrando Criterios de Jerarquización

--- src/app/componentes/jerarquizacion/formulario-jerarquizacion/formulario-jerarquizacion.component.ts
+++ src/app/componentes/jerarquizacion/formulario-jerarquizacion/formulario-jerarquizacion.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { DatePipe } from '@angular/common'
 import { Subject } from 'rxjs';
@@ -11,14 +11,47 @@ const now = new Date();
   styleUrls: ['./formulario-jerarquizacion.component.scss']
 })
 export class FormularioJerarquizacionComponent implements OnInit {
-  lnacion = ['', 'GT', 'US'];
-  lnestim= ['', '', ''];
-  lnabund= ['', '', ''];
-  lndist= ['', '', ''];
-  lnleprot= ['', '', ''];
-  lnamenaz= ['', '', ''];
-  lrangon= ['', '', ''];
-  
+
+  lestim = ['',
+    '0 - 5 localizaciones conocidas',
+    '6 - 20 localizaciones conocidas',
+    '21 - 100 localizaciones conocidas',
+    '100 o más localizaciones conocidas'];
+  labund = ['',
+    '< 1000 individuos (PE Y AE), 1000 hectáreas (CN), 30 Km a lo largo de un río (CN) ',
+    '= 1000 - 3000 individuos, 1000-5000 hectáreas, 30-10 Km',
+    '= 3000 - 10,000 individuos, 5000-25,000 hectareás, 100-500 Km',
+    '> 10,000 individuos , 25,000 hectáreas, 500 kilómetros'];
+
+  lgdist = ['', 'Endémica de un país, departamento grande o región; una área < 260 km²',
+    'Edémica regional; una área de 260 - 26,000 km²',
+    'Moderadamente ampia pero dispersa; una área de 26,000 - 2,600,000 km²',
+    'Amplia; una área > 2,600,000 km²'];
+  lndist = ['',
+    'Muy limitada; porcentaje de territorio nacional < 3%',
+    'Limitada; porcentaje de territorio nacional 3 - 10%',
+    'Moderadamente amplia; porcentaje de territorio nacional 10 - 50%',
+    'Amplia; porcentaje de territorio nacional > 50%'];
+
+  lleprot = ['',
+    'Ninguna LE protegida (hasta dónde se conoce)',
+    'Por lo menos una LE protegida',
+    'Varias LE protegidas',
+    'Muchas LE protegidas',
+    'Se ignora'];
+  lamenaz = ['',
+    'Muy amenazado: El elemento es directamente explotado o amenazado por causas naturalez o por el hombre',
+    'Moderadamente amenazado: El habitat o comunidad se presta para usos alternos que amenazan al elemento',
+    'Ligeramente amenazado: El habitat o comunidad se protege ya que no se presta para usos que amenazan al elemento',
+    'Sin amenaza'];
+  lgfragil = ['',
+    'Muy frágil (murciélago en cueva de maternidad)',
+    'Frágil (Felis spp.)',
+    'Bastante resistente (perezoso de tres dedos)',
+    'Resistente (algunas ardillas)'];
+  lrango = ['', '', ''];
+  val: number;
+
   jerarquizacionForm: FormGroup;
   date: { year: number, month: number };
   modelDate: NgbDateStruct;
@@ -62,6 +95,27 @@ export class FormularioJerarquizacionComponent implements OnInit {
       'actualizar': ''
     });
   }
+  // this.registroForm.get('fechaNacimiento').value;
+  onSubmit() {
+    console.log('Criterio ', this.jerarquizacionForm.get('nlestim').value);
+
+  }
+  /**Criterios de Jerarquización  
+   *   Es comun y con diferente significado segun sea el caso para
+   *   GLESTIM, NLESTIM, GABUND, NABUND, GDIST, NDIST, 
+   *   GLEPROT, NLEPROT, GAMENAZ, NAMENAZ, GFRAGIL
+   * **/
+  getCriterio_Jerarquizacion(i: number) {
+    switch (i) {
+      case 0: return '';
+      case 1: return 'A';
+      case 2: return 'B';
+      case 3: return 'C';
+      case 4: return 'D';
+      case 5: return 'I';
+    }
+  }
+
   selectToday() {
     this.modelDate = { year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate() };
   }
