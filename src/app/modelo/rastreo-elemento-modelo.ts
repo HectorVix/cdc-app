c61c3780c820d28948cd63d50d4b9b4d9fc4f0a7
Hector <vix.hector@gmail.com>
2018-08-15 07:32:49 -0600

    Modelo Rastreo  del Elemento

---
+++ src/app/modelo/rastreo-elemento-modelo.ts
@@ -0,0 +1,77 @@
+export class rastreo_Elemento_Modelo {
+ //pagina1
+      //identificadores
+      codigoe:          String;
+      tropicos:         String;
+      nacion:           String;
+      subnacion:        String;
+      //taxonomia (global)
+      clasetax:         String;
+      orden:            String;
+      familia:          String;
+      genero:           String;
+      nombreg:          String;
+      autor:            String;
+      fuentenom:        Number;
+      refnombreg:       String;
+      disttax:          String;
+      dudatax:          String;
+      nomcomung:        String;
+      comtaxg:          String;
+      //taxonomia (nacional)
+      nombren:          String;
+      numsinn:          Number;
+      nomcomunn:        String;
+      comtaxn:          String;
+      //status (global)
+      rangog:           String;
+      fecharevrg:       Date;
+      formularg:        String;
+      resprg:           String;
+      aepeu:            String;
+      fechaaepeu:       Date;
+      cites:            String;
+      iucn:             String;
+      planscons:        String;
+      resplan:          String;
+      resumenman:       String;
+      resresumen:       String;
+      exsitu:           String;
+      instexsitu:       String;
+      endemismo:        String;
+      //status (nacional)
+      rangon:           String;
+      fecharevrn:       Date;
+      formularn:        String;
+      rastreolen:       String;
+      lestimn:          String;
+      leprotn:          String;
+      abundn:           String;
+      protnacion:       String;
+      refnombren:       String;
+      transparencian:   String;
+      //status (subnacional)
+      rangos:           String;
+      fecharevrs:       Date;
+      formulars:        String;
+      rastreoles:       String;
+      lestims:          String;
+      leprots:          String;
+      abunds:           String;
+      protsubnac:       String;
+      refnombres:       String;
+      transparencias:   String;
+      //campos opcionales
+      reopc1:           String;
+      reopc2:           String;
+      reopc3:           String;
+      reopc4:           String;
+      reopc5:           String;
+      // manteniiento del registro
+      codfuenten:       String;
+      codfuentes:       String;
+      actualizag:       Date;
+      actualizan:       Date;
+      actualizas:       Date;
+
+}
