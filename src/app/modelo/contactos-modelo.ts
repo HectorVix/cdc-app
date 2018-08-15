1e17b792af48e96e2716b4610d1d9a817d0c92a7
Hector <vix.hector@gmail.com>
2018-08-15 14:52:41 -0600

    Modelo Contactos

--- src/app/modelo/contactos-modelo.ts
+++ src/app/modelo/contactos-modelo.ts
@@ -1,3 +1,34 @@
 export class contactos_Modelo {
-    
+   //identificadores
+   numident:     String;
+   nombreident:  String;
+   titulo:       String;
+   nombre:       String;
+   apellido1:    String;
+   apellido2:    String;
+   sufijo:       String;
+   posicion:     String;
+   institucion:  String;
+   //localizadores
+   email:        String;
+   dir1:         String;
+   dir2:         String;
+   dir3:         String;
+   pais:         String;
+   ciudad:       String;
+   subnacion:    String;
+   codpostal:    String;
+   masident:     String;
+   smsa:         String;
+   teleftrabajo: Number;
+   telefhogar:   Number;
+   //tipos de contactos
+   tipocont:     String;
+   //actividades con el contacto
+   activcont:    String;
+   //descripción
+   resumen:      String;
+   //documentación y mantenimiento
+   coddirp:      String;
+   actualizar:   Date;  
 }
