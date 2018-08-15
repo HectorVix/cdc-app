e8b9898ffc752f9a620fce809d09a2fad6184281
Hector <vix.hector@gmail.com>
2018-08-15 13:36:08 -0600

    Modelo Caracterizacion Plantas nacional

---
+++ src/app/modelo/resumen-caracterizacion-plantas-nacional-modelo.ts
@@ -0,0 +1,102 @@
+export class resumen_Caracterizacion_Plantas_Nacional_Modelo {
+          //identificadores
+          codigoe:     String;
+          nacion:      String;
+          nombren:     String;
+          nomcomunn:   String;
+          //taxonomía  
+          comsubespn:  String;
+          taxasimiln:  String;
+          comidentn:   String;
+          comtaxn:     String;
+          //status     
+          rangog:      String;
+          rangon:      String;
+          aepeu:       String;
+          cites:       String;
+          uicn:        String;
+          rastreolen:  String;
+          protnacion:  String;
+          malezan:     String;
+          clasifinstn: String;
+          comstatn:    String;
+          //inventario 
+          priinventn:  String;
+          necinventn:  String;
+          cominventn:  String;
+          respropn:    String;
+          //lista_distribucion1: 
+          // lista_distribucion2: 
+          elevminn:    Number;
+          elevmaxn:    Number;
+          disyuntn:    String;
+          periferican: String;
+          comdistn:    String;
+          //habitat    
+          marinon:     String;
+          estuarinon:  String;
+          fluvialn:    String;
+          lacustren:   String;
+          palustren:   String;
+          terrestren:  String;
+          comhabn:     String;
+          //ecología   String;
+          comecoln:    String;
+          //fenología
+          //enero
+          nenea1:String;  nenea2:String;  nenea3:String;  nenea4:String; 
+          neneb1:String;  neneb2:String;  neneb3:String;  neneb4:String; 
+          //febrero
+          nfeba1:String;  nfeba2:String;  nfeba3:String;  nfeba4:String; 
+          nfebb1:String;  nfebb2:String;  nfebb3:String;  nfebb4:String; 
+          //marzo
+          nmara1:String;  nmara2:String;  nmara3:String;  nmara4:String; 
+          nmarb1:String;  nmarb2:String;  nmarb3:String;  nmarb4:String; 
+          //abril
+          nabra1:String;  nabra2:String;  nabra3:String;  nabra4:String; 
+          nabrb1:String;  nabrb2:String;  nabrb3:String;  nabrb4:String; 
+          //mayo
+          nmaya1:String; nmaya2:String; nmaya3:String; nmaya4:String; 
+          nmayb1:String; nmayb2:String; nmayb3:String; nmayb4:String; 
+          //junio
+          njuna1:String; njuna2:String; njuna3:String; njuna4:String; 
+          njunb1:String; njunb2:String; njunb3:String; njunb4:String; 
+          //julio
+          njula1:String; njula2:String; njula3:String; njula4:String; 
+          njulb1:String; njulb2:String; njulb3:String; njulb4:String; 
+          //agosto
+          nagoa1:String; nagoa2:String; nagoa3:String; nagoa4:String; 
+          nagob1:String; nagob2:String; nagob3:String; nagob4:String; 
+          //septiembre
+          nseta1:String; nseta2:String; nseta3:String; nseta4:String; 
+          nsetb1:String; nsetb2:String; nsetb3:String; nsetb4:String;
+          //octubre
+          nocta1:String; nocta2:String; nocta3:String; nocta4:String; 
+          noctb1:String; noctb2:String; noctb3:String; noctb4:String;
+          //noviembre
+          nnova1:String; nnova2:String; nnova3:String; nnova4:String; 
+          nnovb1:String; nnovb2:String; nnovb3:String; nnovb4:String; 
+          //diciembre
+          ndica1:String; ndica2:String; ndica3:String; ndica4:String; 
+          ndicb1:String; ndicb2:String; ndicb3:String; ndicb4:String; 
+          comfenoln:  String;
+          //reproducción
+          comrepn:    String;
+          //manejo
+          commanejon: String;
+          //campos opcionales
+          rcpnopc1:   String;
+          rcpnopc2:   String;
+          rcpnopc3:   String;
+          rcpnopc4:   String;
+          rcpnopc5:   String; 
+          //mantenimiento del registro
+          codfuente:  String;
+          cita:       String;
+          transparen: String;
+          refg:       String;
+          refn:       String;
+          edicionn:   Date;
+          autoredn:   String;
+          actualizan: Date;
+}
--- src/app/modelo/resumen-modelo.ts
+++
@@ -1,3 +0,0 @@
-export class resumen_Modelo {
-    
-}
