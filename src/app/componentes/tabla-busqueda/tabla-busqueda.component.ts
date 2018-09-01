d402a806a6a865f57e406d6a3c212b9eb554b924
Hector <vix.hector@gmail.com>
2018-09-01 13:05:42 -0600

    Pruebas tablas

--- src/app/componentes/tabla-busqueda/tabla-busqueda.component.ts
+++ src/app/componentes/tabla-busqueda/tabla-busqueda.component.ts
@@ -4,12 +4,20 @@ import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 export interface ElementoDato {
   numero: number;
   codigo: String;
-  fecha: Date;
+  fecha: String;
   nombre: String;
   usuario: String;
   comentario: String;
 }
-
+const ELEMENT_DATA: ElementoDato[] = [
+  {numero: 1, codigo: '3480372870101', fecha:'2018-30-1', nombre: 'Hector Vix1 ',usuario: 'HéctorVix1', comentario:''},
+  {numero: 2, codigo: '3480372870102', fecha:'2018-30-1', nombre: 'Hector Vix2',usuario: 'HéctorVix2', comentario:''},
+  {numero: 3, codigo: '3480372870103', fecha:'2018-30-1', nombre: 'Hector Vix3',usuario: 'HéctorVix3', comentario:''},
+  {numero: 4, codigo: '3480372870104', fecha:'2018-30-1', nombre: 'Hector Vix4',usuario: 'HéctorVix4', comentario:''},
+  {numero: 5, codigo: '3480372870105', fecha:'2018-30-1', nombre: 'Hector Vix5',usuario: 'HéctorVix5', comentario:''},
+  {numero: 6, codigo: '3480372870106', fecha:'2018-30-1', nombre: 'Hector Vix6',usuario: 'HéctorVix6', comentario:''},
+  {numero: 7, codigo: '3480372870107', fecha:'2018-30-1', nombre: 'Hector Vix7',usuario: 'HéctorVix7', comentario:''}
+];
 @Component({
   selector: 'app-tabla-busqueda',
   templateUrl: './tabla-busqueda.component.html',
@@ -17,7 +25,8 @@ export interface ElementoDato {
 })
 export class TablaBusquedaComponent implements OnInit {
   displayedColumns: string[] = ['numero', 'codigo', 'fecha', 'nombre', 'usuario'];
-  dataSource: MatTableDataSource<ElementoDato>;
+ /// dataSource: MatTableDataSource<ElementoDato>;
+  dataSource =ELEMENT_DATA;
   elementos: Array<ElementoDato> = new Array();
   private paginator: MatPaginator;
   @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
@@ -26,24 +35,24 @@ export class TablaBusquedaComponent implements OnInit {
   }
   @ViewChild(MatSort) sort: MatSort
   constructor() {
-    this.dataSource = new MatTableDataSource(this.elementos);
+  // this.dataSource = new MatTableDataSource(this.elementos);
   }
 
   ngOnInit() {
-    this.dataSource.sort = this.sort;
+   // this.dataSource.sort = this.sort;
   }
   setDataSourceAttributes() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+ //   this.dataSource.paginator = this.paginator;
+//this.dataSource.sort = this.sort;
     if (this.paginator && this.sort) {
       this.applyFilter('');
     }
   }
   applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
+//    this.dataSource.filter = filterValue.trim().toLowerCase();
+   // if (this.dataSource.paginator) {
+    //  this.dataSource.paginator.firstPage();
+  // }
 
   }
 }
