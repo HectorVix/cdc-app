059165aa844bd904f012364ed6fba075ab2f4605
Hector <vix.hector@gmail.com>
2019-02-19 18:19:07 -0600

    Proxy Configurado

--- src/app/servicios/area/area.service.ts
+++ src/app/servicios/area/area.service.ts
@@ -11,29 +11,29 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class AreaService {
-  readonly rootUrl = 'http://localhost:8080/cdc/rs';
+//  readonly rootUrl = 'http://localhost:8080/cdc/rs';
   constructor(private http: HttpClient) { }
 
   addArea(area: area_Modelo): Observable<respuesta_cdc_Modelo> {
-    return this.http.post<respuesta_cdc_Modelo>(this.rootUrl + '/area/registro', area, httpOptions);
+    return this.http.post<respuesta_cdc_Modelo>('/cecon/area/registro', area, httpOptions);
   }
   addListaElemento(areaId: Number, listaElemento: listaElemento_Modelo) {
-    return this.http.post(this.rootUrl + '/area/registrar/listaElemento/' + areaId, listaElemento, httpOptions);
+    return this.http.post('/area/registrar/listaElemento/' + areaId, listaElemento, httpOptions);
   }
   //Obtener áreas por codigoam, nombream, sinam, codsitio, nomsitio, nación, subnación y subdivisión
   getAreas(a: String, b: String, c: String, d: String, e: String, f: String, g: String, h: String): Observable<area_Modelo> {
-    return this.http.get<area_Modelo>(this.rootUrl + '/area/buscar/' + a + '/' + b + '/' + c + '/' + d + '/' + e + '/' + f + '/' + g + '/' + h);
+    return this.http.get<area_Modelo>('/cecon/area/buscar/' + a + '/' + b + '/' + c + '/' + d + '/' + e + '/' + f + '/' + g + '/' + h);
   }
   getListaElemento(areaId: Number): Observable<listaElemento_Modelo> {
-    return this.http.get<listaElemento_Modelo>(this.rootUrl + '/area/listaElemento/' + areaId);
+    return this.http.get<listaElemento_Modelo>('/area/listaElemento/' + areaId);
   }
   updateArea(area: area_Modelo): Observable<respuesta_cdc_Modelo> {
-    return this.http.post<respuesta_cdc_Modelo>(this.rootUrl + '/area/editar', area, httpOptions);
+    return this.http.post<respuesta_cdc_Modelo>('/area/editar', area, httpOptions);
   }
   updateListaElemento(areaId: Number, listaElemento: listaElemento_Modelo) {
-    return this.http.post(this.rootUrl + '/area/update/listaElemento/' + areaId, listaElemento, httpOptions);
+    return this.http.post('/area/update/listaElemento/' + areaId, listaElemento, httpOptions);
   }
   deleteListaElemento(listaElemento_Id: Number) {
-    return this.http.post(this.rootUrl + '/area/delete/listaElemento/' + listaElemento_Id, httpOptions);
+    return this.http.post('/area/delete/listaElemento/' + listaElemento_Id, httpOptions);
   }
 }
